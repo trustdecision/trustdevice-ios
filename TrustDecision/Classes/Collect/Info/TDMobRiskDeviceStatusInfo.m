d5e5708ea94c37e13be6cd8778b6ce82b66c620d
zeinber <442196841@qq.com>
2023-05-18 11:49:49 +0800

    feat：update simulator check

--- TrustDecision/Classes/Collect/Info/TDMobRiskDeviceStatusInfo.m
+++ TrustDecision/Classes/Collect/Info/TDMobRiskDeviceStatusInfo.m
@@ -104,11 +104,36 @@
 
 - (int)isSimulator {
     int simulator = -1;
+    {
 #if TARGET_IPHONE_SIMULATOR
-    simulator = 1;
+        simulator = 1;
 #else
-    simulator = 0;
+        simulator = 0;
 #endif
+    }
+    if (!simulator) {
+        uint64_t dyld_simulator = 0;
+        uint32_t dyld_count = _dyld_image_count();
+        NSString* dyld_sim_str = @"usr/lib/dyld_sim";
+        NSString* CoreSimulator_str = @"Library/Developer/CoreSimulator";
+        char* cEmptyString = "";
+        for (uint32_t i = 0; i < dyld_count; i++) {
+            @autoreleasepool {
+                if(i > 10000)
+                    break;
+                NSString *dyld_image_name = [NSString stringWithCString:_dyld_get_image_name(i) ?: cEmptyString encoding:NSUTF8StringEncoding];
+                //dyld_simulator
+                if ([dyld_image_name hasSuffix:dyld_sim_str]) {
+                    dyld_simulator |= 1;
+                }
+                if ([dyld_image_name containsString:CoreSimulator_str]) {
+                    dyld_simulator |= 1<<1;
+                }
+            }
+        }
+        simulator = (dyld_simulator ? 1 : 0);
+    }
     return simulator;
+    
 }
 @end
