0b90bbdc777518866e487068d93ab52b007f66c7
zeinber <442196841@qq.com>
2025-05-13 15:33:41 +0800

    feat：adapt spm

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
             name: "TrustDecision",
             targets: ["TrustDecision"]),
     ],
-    // dependencies: [
-    //     .package(url: "https://github.com/trustdecision/trustdevice-ios.git",  
-    //     from: "1.6.0"),
-    // ],
+    dependencies: [
+        .package(url: "https://github.com/trustdecision/trustdevice-ios.git",  
+        from: "1.6.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
